e0a3eaf89afd3518792e46c2ca930fde31542eb5
Jered <94761000+JEREDC@users.noreply.github.com>
2022-06-08 10:37:35 -1000

    Update FavoriteWorkshops_SelectAll.sql

--- Sql/Programmability/Stored-Procedures/FavoriteWorkshops_SelectAll.sql
+++ Sql/Programmability/Stored-Procedures/FavoriteWorkshops_SelectAll.sql
@@ -1,15 +1,15 @@
 ALTER Proc [dbo].[FavoriteWorkshops_SelectAll]
-			@PageIndex int,
-			@PageSize int
+		@PageIndex int,
+		@PageSize int
 
 /* ------------- TEST CODE ---------------------
 
 	Declare @PageIndex int =  0,
-			@PageSize int = 5
+		@PageSize int = 5
 
 	Execute dbo.FavoriteWorkshops_SelectAll
-				@PageIndex,
-				@PageSize
+		@PageIndex,
+		@PageSize
 
 */
 
@@ -19,60 +19,60 @@ BEGIN
 
 	Declare @offset int = @PageIndex * @PageSize
 
-		Select ws.[Id], COUNT(ws.Id) AS 'TotalFavorited'
-			,ws.[Name]
-			,ws.[Summary]
-			,ws.[ShortDescription]
-			,ws.[VenueId]
-			,u.Id
-			,u.FirstName 
-			,u.LastName
-			,u.AvatarUrl
-			,wst.Name as workShopType
-			,wss.Name workShopStatus
-			,ws.[ImageUrl]
-			,ws.[ExternalSiteUrl]
-			,ws.[LanguageId]
-			,ws.[IsFree]
-			,ws.[NumberOfSessions]
-			,ws.[DateStart]
-			,ws.[DateEnd]
-			,ws.[DateCreated]
-			,ws.[DateModified]
-			,TotalCount = COUNT(1) OVER()
+	Select ws.[Id], COUNT(ws.Id) AS 'TotalFavorited'
+		,ws.[Name]
+		,ws.[Summary]
+		,ws.[ShortDescription]
+		,ws.[VenueId]
+		,u.Id
+		,u.FirstName 
+		,u.LastName
+		,u.AvatarUrl
+		,wst.Name as workShopType
+		,wss.Name workShopStatus
+		,ws.[ImageUrl]
+		,ws.[ExternalSiteUrl]
+		,ws.[LanguageId]
+		,ws.[IsFree]
+		,ws.[NumberOfSessions]
+		,ws.[DateStart]
+		,ws.[DateEnd]
+		,ws.[DateCreated]
+		,ws.[DateModified]
+		,TotalCount = COUNT(1) OVER()
 			
 
 	From dbo.FavoriteWorkshops as f LEFT JOIN dbo.WorkShop as ws
-								on f.WorkShopId = ws.Id
-								INNER JOIN dbo.Venues as v
-								on ws.VenueId = v.Id
-								INNER JOIN dbo.WorkShopStatus as wss
-								on ws.WorkShopStatusId = wss.Id
-								INNER JOIN dbo.WorkShopTypes as wst
-								on ws.WorkShopTypeId = wst.Id
-								INNER JOIN dbo.UserProfiles as u
-								on ws.HostId = u.UserId
+						on f.WorkShopId = ws.Id
+						INNER JOIN dbo.Venues as v
+						on ws.VenueId = v.Id
+						INNER JOIN dbo.WorkShopStatus as wss
+						on ws.WorkShopStatusId = wss.Id
+						INNER JOIN dbo.WorkShopTypes as wst
+						on ws.WorkShopTypeId = wst.Id
+						INNER JOIN dbo.UserProfiles as u
+						on ws.HostId = u.UserId
 
 	Group by ws.[Id]
-			,ws.[Name]
-			,ws.[Summary]
-			,ws.[ShortDescription]
-			,ws.[VenueId]
-			,u.Id
-			,u.FirstName 
-			,u.LastName
-			,u.AvatarUrl
-			,wst.Name 
-			,wss.Name
-			,ws.[ImageUrl]
-			,ws.[ExternalSiteUrl]
-			,ws.[LanguageId]
-			,ws.[IsFree]
-			,ws.[NumberOfSessions]
-			,ws.[DateStart]
-			,ws.[DateEnd]
-			,ws.[DateCreated]
-			,ws.[DateModified]
+		,ws.[Name]
+		,ws.[Summary]
+		,ws.[ShortDescription]
+		,ws.[VenueId]
+		,u.Id
+		,u.FirstName 
+		,u.LastName
+		,u.AvatarUrl
+		,wst.Name 
+		,wss.Name
+		,ws.[ImageUrl]
+		,ws.[ExternalSiteUrl]
+		,ws.[LanguageId]
+		,ws.[IsFree]
+		,ws.[NumberOfSessions]
+		,ws.[DateStart]
+		,ws.[DateEnd]
+		,ws.[DateCreated]
+		,ws.[DateModified]
 
 	Order by TotalFavorited DESC
 
