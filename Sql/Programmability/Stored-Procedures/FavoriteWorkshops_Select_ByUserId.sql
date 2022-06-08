6ed5b71bd30feafa84a333d0775d5e67d0cb9f17
Jered <94761000+JEREDC@users.noreply.github.com>
2022-06-08 10:34:53 -1000

    Update FavoriteWorkshops_Select_ByUserId.sql

--- Sql/Programmability/Stored-Procedures/FavoriteWorkshops_Select_ByUserId.sql
+++ Sql/Programmability/Stored-Procedures/FavoriteWorkshops_Select_ByUserId.sql
@@ -1,18 +1,18 @@
 ALTER Proc [dbo].[FavoriteWorkshops_Select_ByUserId]
-			@UserId int,
-			@PageIndex int,
-			@PageSize int
+		@UserId int,
+		@PageIndex int,
+		@PageSize int
 
 /* ------------- TEST CODE ---------------------
 
 	Declare @UserId int = 261,
-			@PageIndex int =  0,
-			@PageSize int = 5
+		@PageIndex int =  0,
+		@PageSize int = 5
 
 	Execute dbo.FavoriteWorkshops_Select_ByUserId
-				@UserId,
-				@PageIndex,
-				@PageSize
+		@UserId,
+		@PageIndex,
+		@PageSize
 
 */
 
@@ -22,38 +22,38 @@ BEGIN
 
 	Declare @offset int = @PageIndex * @PageSize
 
-		Select ws.[Id]
-			,ws.[Name]
-			,ws.[Summary]
-			,ws.[ShortDescription]
-			,ws.[VenueId]
-			,u.Id
-			,u.FirstName 
-			,u.LastName
-			,u.AvatarUrl
-			,wst.Name as workShopType
-			,wss.Name workShopStatus
-			,ws.[ImageUrl]
-			,ws.[ExternalSiteUrl]
-			,ws.[LanguageId]
-			,ws.[IsFree]
-			,ws.[NumberOfSessions]
-			,ws.[DateStart]
-			,ws.[DateEnd]
-			,ws.[DateCreated]
-			,ws.[DateModified]
-			,TotalCount = COUNT(1) OVER()
+	Select ws.[Id]
+		,ws.[Name]
+		,ws.[Summary]
+		,ws.[ShortDescription]
+		,ws.[VenueId]
+		,u.Id
+		,u.FirstName 
+		,u.LastName
+		,u.AvatarUrl
+		,wst.Name as workShopType
+		,wss.Name workShopStatus
+		,ws.[ImageUrl]
+		,ws.[ExternalSiteUrl]
+		,ws.[LanguageId]
+		,ws.[IsFree]
+		,ws.[NumberOfSessions]
+		,ws.[DateStart]
+		,ws.[DateEnd]
+		,ws.[DateCreated]
+		,ws.[DateModified]
+		,TotalCount = COUNT(1) OVER()
 
 	From dbo.FavoriteWorkshops as f LEFT JOIN dbo.WorkShop as ws
-								on f.WorkShopId = ws.Id
-								INNER JOIN dbo.Venues as v
-								on ws.VenueId = v.Id
-								INNER JOIN dbo.WorkShopStatus as wss
-								on ws.WorkShopStatusId = wss.Id
-								INNER JOIN dbo.WorkShopTypes as wst
-								on ws.WorkShopTypeId = wst.Id
-								INNER JOIN dbo.UserProfiles as u
-								on ws.HostId = u.UserId
+						on f.WorkShopId = ws.Id
+						INNER JOIN dbo.Venues as v
+						on ws.VenueId = v.Id
+						INNER JOIN dbo.WorkShopStatus as wss
+						on ws.WorkShopStatusId = wss.Id
+						INNER JOIN dbo.WorkShopTypes as wst
+						on ws.WorkShopTypeId = wst.Id
+						INNER JOIN dbo.UserProfiles as u
+						on ws.HostId = u.UserId
 
 	Where f.UserId = @UserId
 	
