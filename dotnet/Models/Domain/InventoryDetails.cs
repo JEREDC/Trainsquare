993d9d3387381a30cbb421d9a0f059bf0a1c7f71
Jered <94761000+JEREDC@users.noreply.github.com>
2022-06-26 07:53:54 -1000

    Create InventoryDetails.cs

---
+++ dotnet/Models/Domain/InventoryDetails.cs
@@ -0,0 +1,18 @@
+public class InventoryDetails
+    {
+        public int Id { get; set; }
+        
+        public WorkShop Workshop { get; set; }
+        
+        public int Quantity { get; set; }
+        
+        public decimal BasePrice { get; set; }
+        
+        public DateTime DateCreated { get; set; }
+        
+        public DateTime DateModified { get; set; }
+        
+        public int CreatedBy { get; set; }
+        
+        public int ModifiedBy { get; set; }
+    }
